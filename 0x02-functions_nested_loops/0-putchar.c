c58d8a60a24dbb6616e948f0a8a8491310ecf464
Realslimshady12 <r7783161@gmail.com>
2023-03-21 13:32:28 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
-#includ "main.h"
-void main ()
+#include "main.h"
+int main ()
 {
-  _putchar('_putchar\n')
-    return (0);
+  _putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 	}
